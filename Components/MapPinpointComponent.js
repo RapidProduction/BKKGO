331b80dba84fb45675a69994aa9ef1dcd8158ffb
Max <patthaphol.max@gmail.com>
2016-11-21 00:16:34 +0700

    Implement OK button on MapPinpoint.

--- Components/MapPinpointComponent.js
+++ Components/MapPinpointComponent.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
 import Pinpoint from './PinpointComponent.js';
 import MapView from 'react-native-maps';
 
 export default class MapPinpoint extends Component {
     constructor(props) {
         super(props);
-        this.state = {enableToPin: false, 
-            enableFromPin: false,
+        this.state = {
+            selectionStep: "to",
             ToLocation: null,
-            Fromlocation: null};
+            Fromlocation: null
+        };
+    }
+
+    onTouchConfirmButton() {
+        var myState = deepCopy(this.state);
+        if (this.state.selectionStep == "to") {
+            myState.selectionStep = "from";
+            this.setState(myState);
+        }
     }
 
     render() {
+        var pinpoint = undefined;
+        if (this.state.selectionStep == "to") {
+            pinpoint = <Pinpoint type = "to" size = "default" style={styles.pinpoint}/>
+        }
+        else if (this.state.selectionStep == "from") {
+            pinpoint = <Pinpoint type = "from" size = "default" style={styles.pinpoint}/>
+        }
+
+        // var confirmButton = undefined;
+        // if (this.state.selectionStep == "to" || this.state.selectionStep == "from") {
+        //     confirmButton = <Button onPress={this.onTouchConfirmButton} title="OK" color="#841584" accessibilityLabel="Touch to confirm selection" />
+        // }
+
         return (
             <View style={styles.container}>
                 <MapView
@@ -23,7 +45,23 @@ export default class MapPinpoint extends Component {
                         latitudeDelta: 0.0922,
                         longitudeDelta: 0.0421,
                     }}
-                />
+                >
+                    <MapView.Marker
+                        coordinate={marker.latlng}
+                        title={marker.title}
+                        description={marker.description}
+                    />
+                </MapView>
+                <View style={styles.pinpoint}>
+                    {pinpoint}
+                </View>
+                <View style={styles.confirmButton}>
+                    <Button
+                        title="OK"
+                        color="#841584"
+                        accessibilityLabel="Touch to confirm selection"
+                    />
+                </View>
             </View>
         );
     }
@@ -51,5 +89,26 @@ var styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#000000',
         flex: 1,
+    },
+    confirmButton: {
+        position: 'absolute',
+        height: 40,
+        width: 40,
+        margin: 2,
+        borderWidth: 1,
+        borderColor: '#000000',
+        bottom: 20,
+        right: 20,
     }
   });
+
+  var marker = {
+        latlng: {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+        },
+        title: 'My Place',
+        description: 'This is my place'
+    };
--- Components/PinpointComponent.js
+++ Components/PinpointComponent.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Image } from 'react-native';
 
+let pinImages = {
+    to:  require('../Assets/Images/PinButton_To.png'),
+    from: require('../Assets/Images/PinButton_To.png')
+}
+
+let pinSize = {
+    small: 30,
+    default: 40,
+    large:  50
+}
+
 export default class Pinpoint extends Component {
     constructor(props) {
         super(props);
-
-        //Props contain
-        //this.props.text
         this.state = {isDropAlpha: false}
     }
 
+    ToggleVisibility() {
+        this.state.isDropAlpha = true;
+    }
+
     render() {
+        var imageSource = pinImages.to;
+        if (this.props.type == "to") {
+            imagePath = pinImages.to;
+        }
+        else if (this.props.type == "from") {
+            imagePath = pinImages.from;
+        }
+
+        var imageSize = pinSize.default;
+        if (this.props.size == "small") {
+            imageSize = pinSize.small;
+        }
+        else if (this.props.size == "large") {
+            imageSize = pinSize.large;
+        }
+
         return (
             <Image
-                style={{width:40, height:40}}
-                source={require('../Assets/Images/PinButton_To.png')}
+                style={{width:imageSize, height:imageSize}}
+                source={imageSource}
             />
         );
     }
